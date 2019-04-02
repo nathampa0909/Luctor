2472123d64e0c5c2bf45b75025a5360ee9bd8229
BuildTools <unconfigured@null.spigotmc.org>
2019-04-01 23:54:42 -0300

    Nova release espaco de eventos

--- espaco-eventos/data.js
+++ espaco-eventos/data.js
@@ -98,7 +98,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
